c6a62110cbde89c4980c737c60d85357dea4f824
chadbartel <chadbartel@users.noreply.github.com>
2019-11-08 19:33:33 -0800

    Initial commit

---
+++ Titanic/helpers.py
@@ -0,0 +1,39 @@
+#! usr/env/bin python
+
+from pandas import read_csv, Categorical
+from numpy import int32, float64
+
+
+class TitanicData:
+
+    def __init__(self, data=None, filepath=None):
+        self.data = data
+        self.filepath = filepath
+    
+    def read_file(self, filepath=None):
+        if self.filepath is None and filepath:
+            self.filepath = filepath
+        
+        if self.filepath.endswith('.csv'):
+            self.data = read_csv(
+                self.filepath,
+                index_col='PassengerId',
+                usecols=[
+                    'Survived', 'Pclass', 'Name',
+                    'Sex', 'Age', 'SibSp', 
+                    'Parch', 'Fare', 'Embarked'
+                ],
+                dtype={
+                    'Survived': int32, 
+                    'Pclass': int32, 
+                    'Name': str,
+                    'Sex': Categorical(["male", "female"]), 
+                    'Age': float64, 
+                    'SibSp': int32, 
+                    'Parch': int32, 
+                    'Fare': float64, 
+                    'Embarked': Categorical(["C", "Q", "S"])
+                }
+            )
+        else:
+            self.data = None
