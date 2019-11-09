fe5a5d9a6f8289883af72ed464442ae5af4f84f3
chadbartel <chadbartel@users.noreply.github.com>
2019-11-09 14:50:06 -0800

    Revise helper function

--- Titanic/Code/DataPrep/helpers.py
+++ Titanic/Code/DataPrep/helpers.py
@@ -1,18 +1,19 @@
 #! usr/env/bin python
 
-from pandas import read_csv, Categorical
+from pandas import read_csv, CategoricalDtype
 from numpy import int32, float64
 
 titanic_dtypes = {
+    'PassengerId': int32,
     'Survived': int32, 
     'Pclass': int32, 
     'Name': str,
-    'Sex': Categorical(["male", "female"]), 
+    'Sex': CategoricalDtype(["male", "female"]), 
     'Age': float64, 
     'SibSp': int32, 
     'Parch': int32, 
     'Fare': float64, 
-    'Embarked': Categorical(["C", "Q", "S"])
+    'Embarked': CategoricalDtype(["C", "Q", "S"])
 }
 
 def get_titanic_data(train_or_test: str='train'):
