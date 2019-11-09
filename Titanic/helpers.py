7ba7f4334338408fc363104ce002502e1b427bcc
chadbartel <chadbartel@users.noreply.github.com>
2019-11-09 08:46:51 -0800

    Delete titanic class

--- Titanic/helpers.py
+++ Titanic/helpers.py
@@ -30,37 +30,3 @@ def get_titanic_data(train_or_test: str='train'):
         dtype=titanic_dtypes
     )
     return df
-
-class TitanicData:
-
-    def __init__(self, data=None, filepath=None):
-        self.data = data
-        self.filepath = filepath
-    
-    def read_file(self, filepath=None):
-        if self.filepath is None and filepath:
-            self.filepath = filepath
-        
-        if self.filepath.endswith('.csv'):
-            self.data = read_csv(
-                self.filepath,
-                index_col='PassengerId',
-                usecols=[
-                    'Survived', 'Pclass', 'Name',
-                    'Sex', 'Age', 'SibSp', 
-                    'Parch', 'Fare', 'Embarked'
-                ],
-                dtype={
-                    'Survived': int32, 
-                    'Pclass': int32, 
-                    'Name': str,
-                    'Sex': Categorical(["male", "female"]), 
-                    'Age': float64, 
-                    'SibSp': int32, 
-                    'Parch': int32, 
-                    'Fare': float64, 
-                    'Embarked': Categorical(["C", "Q", "S"])
-                }
-            )
-        else:
-            self.data = None
